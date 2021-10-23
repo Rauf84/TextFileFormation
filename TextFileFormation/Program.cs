eeb4bf25b02b75d26d7020f7d8b7b097693e3418
Rauf84 <rauf@karimli.se>
2021-10-23 09:57:10 +0200

    Main menu

---
+++ TextFileFormation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextFileFormation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******MENU******");
+            Console.WriteLine("1. CREATE TEXTFILE C:\\1000_words.txt");
+            Console.WriteLine("2. CREATE TEXTFILE C:\\1500_words.txt");
+            Console.WriteLine("3. CREATE TEXTFILE C:\\3000_words.txt");
+            Console.WriteLine("4. CHOISE TEXT FILE TO CONTINUE");
+            Console.WriteLine("Q. QUIT");
+        }
+
+
+
+        List<string> TextToListConverter (string fileName)
+        {
+            List<string> listOfWords = new List<string>();
+            
+
+            return listOfWords;
+        }
+
+    }
+}
