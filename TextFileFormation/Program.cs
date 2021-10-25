d8eea7156f0d2206a821764db5195c1be9e15712
Dennis Lindquist <dennislindquist@gmail.com>
2021-10-25 21:59:37 +0200

    Made a new class and added some stuff.

--- TextFileFormation/Program.cs
+++ TextFileFormation/Program.cs
@@ -7,23 +7,8 @@ namespace TextFileFormation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("******MENU******");
-            Console.WriteLine("1. CREATE TEXTFILE C:\\1000_words.txt");
-            Console.WriteLine("2. CREATE TEXTFILE C:\\1500_words.txt");
-            Console.WriteLine("3. CREATE TEXTFILE C:\\3000_words.txt");
-            Console.WriteLine("4. CHOISE TEXT FILE TO CONTINUE");
-            Console.WriteLine("Q. QUIT");
+            var wordSearcher = new WordSearcher();
+            wordSearcher.Run();
         }
-
-
-
-        List<string> TextToListConverter (string fileName)
-        {
-            List<string> listOfWords = new List<string>();
-            
-
-            return listOfWords;
-        }
-
     }
 }
